b9a33eaff269c5c2b0df6967ce48b96e02f10b60
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2025-06-23 15:22:40 +0300

    feat(Hide StatusBar): add StatusBar component to control visibility in the layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import {StatusBar} from "react-native";
 
 export default function RootLayout() {
-  return <Stack>
+  return(
+  <>
+    <StatusBar hidden={true} />
+    <Stack>
     <Stack.Screen
       name="(tabs)"
       options={{headerShown: false}}
@@ -12,4 +16,5 @@ export default function RootLayout() {
       options={{headerShown: false}}
     />
   </Stack>;
+  </>)
 }
